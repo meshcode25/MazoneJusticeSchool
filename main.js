251bf63c5313c867eaa0e824f9faba89dea0087d
Yegon Kiprotich <meshcode75@gmail.com>
2023-12-20 01:57:00 +0300

    first commit for mazone justice school repository

---
+++ main.js
@@ -0,0 +1,12 @@
+//toggle and untoggle service display on hover
+// var dropdownbutton= document.getElementById("dropDownButton");
+
+// var dropdowncontent=document.getElementById("dropDownContent");
+
+// dropdownbutton.addEventListener("mouseover", ()=>{
+//     dropdowncontent.style.display="none";
+//     dropdowncontent.style.animatae="all 2s ease";
+// });
+// dropdownbutton.addEventListener("mouseout", ()=>{
+//     dropdowncontent.style.display="none";
+// })
