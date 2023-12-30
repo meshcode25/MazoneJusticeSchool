0c914363ae6adf887a8d715834cf743a5504a2fc
Yegon Kiprotich <meshcode75@gmail.com>
2023-12-30 15:42:03 +0300

    Just updated the name to New Mazone and added the director image as well as the brochure image

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ showmenu.addEventListener("click", ()=>{
 
 
     navlinks.setAttribute(
-        "style","position:absolute; top:77.5px; display:block; opacity:1; visibility:visible; background-color: #242526"
+        "style","position:absolute; top:0px; left:0; display:block; opacity:1; visibility:visible; background-color: #242526; width:70%;"
     )
 });
 collapsemenu.addEventListener("click", ()=>{
