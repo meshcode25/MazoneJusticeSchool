9d9a8f4593739b5be9b68b6fa7653d4b4b55c2c6
Yegon Kiprotich <meshcode75@gmail.com>
2024-01-03 15:16:27 +0300

    Just updated the name to New Mazone and added the director image as well as the brochure image

--- main.js
+++ main.js
@@ -34,19 +34,19 @@ var secondarycollapse= document.getElementById("secondarycollapse");
         let prevScrollPos = window.pageYOffset;
 
         window.addEventListener('scroll', function() {
-        // current scroll position
-        const currentScrollPos = window.pageYOffset;
+            // current scroll position
+            const currentScrollPos = window.pageYOffset;
 
-        if (prevScrollPos > currentScrollPos) {
-            // user has scrolled up
-            document.querySelector('nav').classList.add('hidenav');
-        } else {
-            // user has scrolled down
-            document.querySelector('nav').classList.remove('hidenav');
-        }
+            if (prevScrollPos > currentScrollPos) {
+                // user has scrolled up
+                document.querySelector('nav').classList.add('hidenav');
+            } else {
+                // user has scrolled down
+                document.querySelector('nav').classList.remove('hidenav');
+            }
 
-        // update previous scroll position
-        prevScrollPos = currentScrollPos;
+            // update previous scroll position
+            prevScrollPos = currentScrollPos;
         });
 
     }
